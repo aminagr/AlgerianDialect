c46701b1d1a439bea5cf275f0b57a9edc2149821
aminagr <minatub@gmail.com>
2024-09-21 08:40:07 +0100

    First version of the App

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,13 +4,15 @@ import '../styles/Home.css';
 const Home = () => {
     return (
         <div className="home">
-            <h1 className="hero-title">Learn Algerian Dialect Now</h1>
-            <p className="hero-subtitle">
-                Dive into the vibrant world of the Algerian dialect! Discover a fun and interactive way to learn English through engaging lessons and quizzes that make mastering the language enjoyable. Join us and elevate your skills today!
-            </p>
-            <div className="button-container">
-                <div className="card-button courses-button">Courses</div>
-                <div className="card-button quizzes-button">Quizzes</div>
+            <div className="hero-content">
+                <h1 className="hero-title">Learn Algerian Dialect Now</h1>
+                <p className="hero-subtitle">
+                    Dive into the vibrant world of the Algerian dialect! Discover a fun and interactive way to learn English through engaging lessons and quizzes that make mastering the language enjoyable. Join us and elevate your skills today!
+                </p>
+                <div className="button-container">
+                    <div className="card-button courses-button">Courses</div>
+                    <div className="card-button quizzes-button">Quizzes</div>
+                </div>
             </div>
         </div>
     );
