dc161a7a3fde4f0201c3dc1590c06ee3e5fcbcd8
aminagr <minatub@gmail.com>
2024-09-25 07:32:49 +0100

    First version of the App

--- src/pages/QuizPage.jsx
+++ src/pages/QuizPage.jsx
@@ -8,49 +8,49 @@ const translations = {
     welcome: "Bienvenue dans le quiz de langue algérienne",
     title: "Choisissez votre langue",
     difficultyTitle: "Choisissez le niveau de difficulté",
-    level1: "Niveau 1",
-    level2: "Niveau 2",
-    level3: "Niveau 3",
+    niveau1: "Niveau 1",
+    niveau2: "Niveau 2",
+    niveau3: "Niveau 3",
   },
   en: {
     welcome: "Welcome to the Algerian Language Quiz",
     title: "Choose Your Language",
     difficultyTitle: "Choose Difficulty Level",
-    level1: "Level 1",
-    level2: "Level 2",
-    level3: "Level 3",
+    niveau1: "Level 1",
+    niveau2: "Level 2",
+    niveau3: "Level 3",
   },
   es: {
     welcome: "Bienvenido al Quiz de Lengua Argelina",
     title: "Elige tu idioma",
     difficultyTitle: "Elige el nivel de dificultad",
-    level1: "Nivel 1",
-    level2: "Nivel 2",
-    level3: "Nivel 3",
+    niveau1: "Nivel 1",
+    niveau2: "Nivel 2",
+    niveau3: "Nivel 3",
   },
   it: {
     welcome: "Benvenuto al Quiz della Lingua Algerina",
     title: "Scegli la tua lingua",
     difficultyTitle: "Scegli il livello di difficoltà",
-    level1: "Livello 1",
-    level2: "Livello 2",
-    level3: "Livello 3",
+    niveau1: "Livello 1",
+    niveau2: "Livello 2",
+    niveau3: "Livello 3",
   },
   ru: {
     welcome: "Добро пожаловать в викторину по алжирскому языку",
     title: "Выберите свой язык",
     difficultyTitle: "Выберите уровень сложности",
-    level1: "Уровень 1",
-    level2: "Уровень 2",
-    level3: "Уровень 3",
+    niveau1: "Уровень 1",
+    niveau2: "Уровень 2",
+    niveau3: "Уровень 3",
   },
   ar: {
     welcome: "مرحبا بكم في اختبار اللغة الجزائرية",
     title: "اختر لغتك",
     difficultyTitle: "اختر مستوى الصعوبة",
-    level1: "المستوى 1",
-    level2: "المستوى 2",
-    level3: "المستوى 3",
+    niveau1: "المستوى 1",
+    niveau2: "المستوى 2",
+    niveau3: "المستوى 3",
   },
 };
 
@@ -58,16 +58,21 @@ const translations = {
 
 
 const QuizPage = () => {
-  const { language } = useAppContext(); // Get language from context
+  const { language } = useAppContext(); // Obtenez la langue du contexte
   const [startQuiz, setStartQuiz] = useState(false);
   const [difficulty, setDifficulty] = useState('niveau1');
+  const [levels, setLevels] = useState([]);
 
   useEffect(() => {
     const savedDifficulty = localStorage.getItem('difficulty');
     if (savedDifficulty) {
       setDifficulty(savedDifficulty);
     }
-  }, []);
+
+    // Récupérer dynamiquement les niveaux de difficulté
+    const availableLevels = Object.keys(questions[language]);
+    setLevels(availableLevels);
+  }, [language]);
 
   const handleStartQuiz = (selectedDifficulty) => {
     setDifficulty(selectedDifficulty);
@@ -81,16 +86,15 @@ const QuizPage = () => {
         <div className="welcome">
           <h1>{translations[language].welcome}</h1>
           <h2 className='titre-difficulte'>{translations[language].difficultyTitle}</h2>
-          <button className="level-button" onClick={() => handleStartQuiz('niveau1')}>
-          {translations[language].level1}
-        </button>
-        <button className="level-button" onClick={() => handleStartQuiz('niveau2')}>
-          {translations[language].level2}
-        </button>
-        <button className="level-button" onClick={() => handleStartQuiz('niveau3')}>
-          {translations[language].level3}
-        </button>
-
+          {levels.map((level, index) => (
+            <button
+              key={index}
+              className="level-button"
+              onClick={() => handleStartQuiz(level)}
+            >
+              {translations[language][level]} {/* Assurez-vous que les traductions existent pour chaque niveau */}
+            </button>
+          ))}
         </div>
       ) : (
         <Quiz setStartQuiz={setStartQuiz} difficulty={difficulty} />
