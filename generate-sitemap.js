9a84be08b9d476c4df58b42efba6ea9ce9f7d0f6
aminagr <minatub@gmail.com>
2024-09-30 13:06:29 +0100

    added sitemap

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -1,13 +1,13 @@
-const { createSitemap } = require('sitemap');
-const fs = require('fs');
+import { createSitemap } from 'sitemap'; 
+import { writeFileSync } from 'fs';
 
 const sitemap = createSitemap({
-    hostname: 'https://learnalgerian.vercel.app', 
+    hostname: 'https://learnalgerian.vercel.app',
     cacheTime: 600000, 
     urls: [
         { url: '/', changefreq: 'daily', priority: 1.0 },
         { url: '/quiz', changefreq: 'daily', priority: 0.8 },
-        { url: '/quiz/:level', changefreq: 'daily', priority: 0.8 }, 
+        { url: '/quiz/:level', changefreq: 'daily', priority: 0.8 },
         { url: '/courses', changefreq: 'daily', priority: 0.8 },
         { url: '/courses/:courseId', changefreq: 'monthly', priority: 0.6 },
         { url: '/courses/:courseId/:lessonId', changefreq: 'daily', priority: 0.6 },
@@ -16,4 +16,4 @@ const sitemap = createSitemap({
 });
 
 
-fs.writeFileSync('public/sitemap.xml', sitemap.toString());
+writeFileSync('public/sitemap.xml', sitemap.toString());
