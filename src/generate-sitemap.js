3627b0464c39d614826c216d604fad3988a3e73c
aminagr <minatub@gmail.com>
2024-09-30 12:56:27 +0100

    added sitemap

---
+++ src/generate-sitemap.js
@@ -0,0 +1,20 @@
+const { createSitemap } = require('sitemap');
+const fs = require('fs');
+
+const sitemap = createSitemap({
+    hostname: 'https://learnalgerian.vercel.app', 
+    cacheTime: 600000, 
+    urls: [
+        { url: '/', changefreq: 'daily', priority: 1.0 },
+        { url: '/quiz', changefreq: 'daily', priority: 0.8 },
+        { url: '/quiz/:level', changefreq: 'daily', priority: 0.8 }, 
+        { url: '/courses', changefreq: 'daily', priority: 0.8 },
+        { url: '/courses/:courseId', changefreq: 'maily', priority: 0.6 },
+        { url: '/courses/:courseId/:lessonId', changefreq: 'daily', priority: 0.6 },
+        { url: '/search', changefreq: 'daily', priority: 0.6 },
+        
+    ],
+});
+
+
+fs.writeFileSync('public/sitemap.xml', sitemap.toString());
